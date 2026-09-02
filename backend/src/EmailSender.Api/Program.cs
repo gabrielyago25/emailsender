324f4350dd50c3fe74d522b038fc45e404a87c36
Gabriel Hodon <gabrielyago25@gmail.com>
2026-09-02 16:37:02 -0300

    feat: resolve cors

--- backend/src/EmailSender.Api/Program.cs
+++ backend/src/EmailSender.Api/Program.cs
@@ -32,6 +32,12 @@ builder.Services.AddSingleton<EnvioJobStore>();
 builder.Services.AddSingleton<EnvioJobQueue>();
 builder.Services.AddHostedService<EnvioBackgroundService>();
 
+builder.Services.AddCors(options => options.AddPolicy("Frontend", policy => {
+    policy.WithOrigins("http://localhost:5173")
+    .AllowAnyHeader()
+    .AllowAnyMethod();
+}));
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -40,5 +46,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("Frontend");
 app.MapControllers();
 app.Run();
